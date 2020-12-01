1bd0591326105bf10709af50dde9ce00d0705a0c
LEE Seungje <38553239+sonyun247@users.noreply.github.com>
2020-12-01 14:40:10 +0900

    recap number counter

--- src/index.js
+++ src/index.js
@@ -4,21 +4,36 @@ const add = document.getElementById("add");
 const subtract = document.getElementById("subtract");
 const number = document.querySelector("span");
 
+const ADD = "ADD";
+const SUBTRACT = "SUBTRACT";
+
+number.innerText = 0;
+
 const countModifier = (state = 0, action) => {
-  console.log(state, action);
-  if (action.type === "ADD") {
-    return state + 1;
-  } else if (action.type === "SUBTRACT") {
-    return state - 1;
-  } else {
-    return state;
+  switch (action.type) {
+    case ADD:
+      return state + 1;
+    case SUBTRACT:
+      return state - 1;
+    default:
+      return state;
   }
-}
+};
 
 const countStore = createStore(countModifier);
 
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "SUBTRACT" });
+const onChange = () => {
+  number.innerText = countStore.getState();
+}
 
-console.log(countStore.getState());
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: ADD });
+}
+const handleSubtract = () => {
+  countStore.dispatch({ type: SUBTRACT });
+}
+
+add.addEventListener("click", handleAdd);
+subtract.addEventListener("click", handleSubtract);
