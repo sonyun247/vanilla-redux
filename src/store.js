3df7fbc89d0058105eadd38dff0869a519d6c42b
LEE Seungje <38553239+sonyun247@users.noreply.github.com>
2020-12-04 15:25:49 +0900

    configure store

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { createStore } from "redux";
-import { createAction, createReducer } from "@reduxjs/toolkit";
+import { configureStore, createAction, createReducer, createSlice } from "@reduxjs/toolkit";
 
 const ADD = "ADD";
 const DELETE = "DELETE";
@@ -15,11 +15,27 @@ const reducer = createReducer([], {
     state.filter((toDo) => toDo.id !== action.payload)
 });
 
-const store = createStore(reducer);
+// const toDos = createSlice({
+//   name: "toDosReducer",
+//   initialState: [],
+//   reducers: {
+//     addToDo: (state, action) => {
+//       state.push({ text: action.payload, id: Date.now() });
+//     },
+//     removeToDo: (state, action) =>
+//       state.filter((toDo) => toDo.id !== action.payload)
+//   }
+// });
+
+const store = configureStore({ reducer });
 
 export const actionCreators = {
   addToDo,
   deleteToDo,
 };
 
+// export const { addToDo, removeToDo } = toDos.actions;
+
 export default store;
+
+// export default configureStore({ reducer: toDos.reducer });
