412668a8dca9df573b0e4579adba7a663edb2e2b
LEE Seungje <38553239+sonyun247@users.noreply.github.com>
2020-12-04 14:39:08 +0900

    apply redux toolkit
    convert to createAction, createReducer

--- src/store.js
+++ src/store.js
@@ -1,26 +1,19 @@
 import { createStore } from "redux";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 const ADD = "ADD";
 const DELETE = "DELETE";
 
-const addToDo = (text) => {
-  return { type: ADD, text };
-};
+const addToDo = createAction("ADD");
+const deleteToDo = createAction("DELETE");
 
-const deleteToDo = (id) => {
-  return { type: DELETE, id: parseInt(id) };
-};
-
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE:
-      return state.filter((toDo) => toDo.id !== action.id);
-    default:
-      return state;
-  }
-};
+const reducer = createReducer([], {
+  [addToDo]: (state, action) => {
+    state.push({ text: action.payload, id: Date.now() });
+  },
+  [deleteToDo]: (state, action) =>
+    state.filter((toDo) => toDo.id !== action.payload)
+});
 
 const store = createStore(reducer);
 
